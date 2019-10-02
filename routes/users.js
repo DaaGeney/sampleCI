1528600a7fe020464aec5ea446f9cfc6818526c5
DaaGeney <diegoassia@gmail.com>
2019-10-01 19:58:59 -0500

    Delete

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,5 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 module.exports = router;
