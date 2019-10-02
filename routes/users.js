f13bd7a5d241684e45fb6e3b1d8e18bf1965dfb0
DaaGeney <diegoassia@gmail.com>
2019-10-01 19:55:42 -0500

    code three

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 module.exports = router;
